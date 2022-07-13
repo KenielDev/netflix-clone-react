f5414591f55f1c5df08386b0a62c8d1ba69b5b60
KenielDev <103508290+KenielDev@users.noreply.github.com>
2022-07-13 17:29:39 -0300

    coding

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import "./api.jsx";
 import Row from "./components/movieRows";
 import categories from "./api.jsx";
 import Banner from "./components/banner";
+import Navbar from "./components/navbar";
 
 <script src="https://cdn.tailwindcss.com"></script>;
 
 function App() {
   return (
     <div className="App text-center bg-black">
-      {/* navbar */}
+      <Navbar />
       <Banner />
       {categories.map((category, index) => {
         return (
